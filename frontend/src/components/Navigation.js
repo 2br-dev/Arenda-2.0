6d24e0472eff35810c2cc83dbeacba0b40b67b5d
Serh037 <prog@2-br.ru>
2019-01-17 21:02:14 +0300

    добавлены стили к хэдэру

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import React, { Component } from 'react'
+import './css/Navigation.css'
 
 // компонент навигации
 // навигация и больше ничего, лаконично и красиво.
@@ -7,22 +8,26 @@ import React, { Component } from 'react'
 export default class Navigation extends Component {
   render() {
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to='/reestr' onClick={() => this.props.handleChangeUrl('reestr')}>Реестр</Link>
-          </li>
-          <li>
-            <Link to='/bills' onClick={() => this.props.handleChangeUrl('bills')}>Выставление счетов</Link>
-          </li>
-          <li>
-            <Link to='/print' onClick={() => this.props.handleChangeUrl('print')}>Печать счетов</Link>
-          </li>
-          <li>
-            <Link to='/payments'onClick={() => this.props.handleChangeUrl('payments')}>Оплаты</Link>
-          </li> 
-        </ul>
-      </nav>
+      <header>
+        <input type="checkbox" class="hamburger" id="hamburger" />
+        <label for="hamburger" class="icon"></label>
+        <nav role='navigation'>
+          <ul>
+            <li>
+              <Link to='/reestr' onClick={() => this.props.handleChangeUrl('reestr')}>Реестр арендаторов</Link>
+            </li>
+            <li>
+              <Link to='/bills' onClick={() => this.props.handleChangeUrl('bills')}>Выставление счетов</Link>
+            </li>
+            <li>
+              <Link to='/print' onClick={() => this.props.handleChangeUrl('print')}>Печать счетов</Link>
+            </li>
+            <li>
+              <Link to='/payments'onClick={() => this.props.handleChangeUrl('payments')}>Оплаты</Link>
+            </li> 
+          </ul>
+        </nav>
+      </header>
     )
   }
 }
